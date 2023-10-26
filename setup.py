da9a15bf7f3c8a950bb038a34b86ca12d307a086
Unnecessaryss <144588578+unnecessaryss@users.noreply.github.com>
2023-10-26 13:17:31 +0300

    New commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_namespace_packages
+
+
+with open('README.md', "r", encoding='UTF-8') as readMeFile:
+    readMe = readMeFile.read()
+
+requirements = ['aiohttp', 'typing', 'pydantic']
+
+setup(
+    name="pykeksikapi",
+    version="0.15",
+    author="unneccessaryss",
+
+    description="This library is needed for working with api.keksik.io. It provides convenient tools for processing the API of this website.",
+    long_description=readMe,
+    long_description_content_type="text/markdown",
+    url="https://github.com/unnecessaryss/pykeksikapi",
+
+    packages=find_namespace_packages(),
+    install_requires=requirements,
+    python_requires=">=3.9"
+)
